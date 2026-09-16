dc58d9a483b95a04a630d39542193ea3c6304f44
Thiru <thirusri270504@gmail.com>
2026-09-16 22:35:19 +0530

    Add files via upload

---
+++ Day 15 Traffic light/script.js
@@ -0,0 +1,12 @@
+function red(){
+    let red =document.getElementById("one").innerHTML="stop";
+    document.body.style.background="red";
+}
+function yellow(){
+    let red =document.getElementById("one").innerHTML="wait";
+    document.body.style.background="yellow";
+}
+function green(){
+    let red =document.getElementById("one").innerHTML="go";
+    document.body.style.background="green";
+}
