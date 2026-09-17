186ec67ce32dae61c23a770f4337cfedc7e44b83
Thiru <thirusri270504@gmail.com>
2026-09-17 12:44:31 +0530

    Create script.js

---
+++ Day 2 Task/script.js
@@ -0,0 +1,12 @@
+function EvenorOdd(){
+let number = 17;
+		if(number%2==0){
+		document.write("Even number");
+		}
+		else if(number%2==1){
+		document.write("Odd number");
+		}	
+    else{
+    document.write("Invalid! number");
+}
+}
