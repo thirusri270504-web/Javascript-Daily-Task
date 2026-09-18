2f4cfae73c0db42f1c6cb9554508c435e320e689
Thiru <thirusri270504@gmail.com>
2026-09-18 21:49:09 +0530

    Create script.js

---
+++ Day 4 Task/script.js
@@ -0,0 +1,15 @@
+	function greater(){
+		a=10;
+		b=20;
+		c=30;
+		if(a>b && a>c){
+		console.log("A is greater number");
+		}
+		else if(b>a && b>c){
+		console.log("B is greater number");
+		}
+		else{
+		console.log("C is greater number");
+		}	
+		}
+		greater();
